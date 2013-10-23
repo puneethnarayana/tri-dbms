4a2de29c0afc074724605b0d7e224418ab7ec1d9
ravindrabhavanam@gmail.com <ravindrabhavanam@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-10-23 17:51:31 +0000

    Directory structure modified.

---
+++ TriDBMS/diskManagement/basicDiskOperations.h
@@ -0,0 +1,17 @@
+/*
+ * basicDiskOperations.h
+ *
+ *  Created on: Oct 23, 2013
+ *      Author: ravin
+ */
+
+#ifndef BASICDISKOPERATIONS_H_
+#define BASICDISKOPERATIONS_H_
+
+class basicDiskOperations {
+public:
+	basicDiskOperations();
+	virtual ~basicDiskOperations();
+};
+
+#endif /* BASICDISKOPERATIONS_H_ */
