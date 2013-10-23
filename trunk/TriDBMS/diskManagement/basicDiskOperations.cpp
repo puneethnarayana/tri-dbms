9f2904a341490d50723f7863c412250783d7dc94
ravindrabhavanam@gmail.com <ravindrabhavanam@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-10-23 17:51:31 +0000

    Directory structure modified.

---
+++ trunk/TriDBMS/diskManagement/basicDiskOperations.cpp
@@ -0,0 +1,18 @@
+/*
+ * basicDiskOperations.cpp
+ *
+ *  Created on: Oct 23, 2013
+ *      Author: ravin
+ */
+
+#include "basicDiskOperations.h"
+
+basicDiskOperations::basicDiskOperations() {
+	// TODO Auto-generated constructor stub
+
+}
+
+basicDiskOperations::~basicDiskOperations() {
+	// TODO Auto-generated destructor stub
+}
+
