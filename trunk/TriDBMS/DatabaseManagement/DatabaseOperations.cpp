1d44a25f32a5f90a77d4b4a8364b74276d64d137
ravindrabhavanam@gmail.com <ravindrabhavanam@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-11-11 19:42:04 +0000

    nothing useful.. Just the create,getters,setters for dbMainHeader

--- trunk/TriDBMS/DatabaseManagement/DatabaseOperations.cpp
+++ trunk/TriDBMS/DatabaseManagement/DatabaseOperations.cpp
@@ -6,6 +6,15 @@
  */
 
 #include "DatabaseOperations.h"
+#include "../HeapFileManagement/DBMainHeaderPage.h"
+#include "../HeapFileManagement/DirectoryHeaderPage.h"
+#include "../HeapFileManagement/DirectoryPage.h"
+#include "../HeapFileManagement/DataPage.h"
+#include "../HeapFileManagement/DirectoryEntry.h"
+#include "../HeapFileManagement/FreePageManager.h"
+#include "../Global/globalDefines.h"
+#include "../Global/globalStructures.h"
+#include "../BufferManagement/BufferManager.h"
 
 DatabaseOperations::DatabaseOperations() {
 	// TODO Auto-generated constructor stub
@@ -16,3 +25,18 @@ DatabaseOperations::~DatabaseOperations() {
 	// TODO Auto-generated destructor stub
 }
 
+int DatabaseOperations::createDatabase(char *databaseName){
+	createDatabase(databaseName,DEFAULT_DB_SIZE);
+}
+
+int DatabaseOperations::createDatabase(char *databaseName,int databaseSize){
+	int fd;
+	int noOfPages=databaseSize*1024*1024/DEFAULT_PAGE_SIZE;
+	buffManager_=BufferManager::getInstance();
+	buffManager_->createDatabase(databaseName,DEFAULT_PAGE_SIZE,noOfPages);
+	fd=buffManager_->openDatabase(databaseName);
+	DBMainHeaderPage *dbMainHeader=new DBMainHeaderPage(fd,0);
+
+
+	return SUCCESS;
+}
--- trunk/TriDBMS/HeapFileManagement/DBMainHeaderPage.cpp
+++ trunk/TriDBMS/HeapFileManagement/DBMainHeaderPage.cpp
@@ -6,9 +6,19 @@
  */
 
 #include "DBMainHeaderPage.h"
+#include "../Global/globalStructures.h"
+#include "../Global/globalDefines.h"
 
-DBMainHeaderPage::DBMainHeaderPage() {
+#include <stdio.h>
+#include <string.h>
+DBMainHeaderPage::DBMainHeaderPage(int fd,int pageNumber) {
 	// TODO Auto-generated constructor stub
+	char *pageData;
+	buffManager_=BufferManager::getInstance();
+	buffManager_->readPage(fd,pageNumber,pageData);
+	memset(&dbMainHeader_,NULL,sizeof(DBMainHeaderStruct));
+	memset(&dbMainHeader_.genPageHeader_,NULL,sizeof(dbMainHeader_.genPageHeader_));
+	memcpy(&dbMainHeader_,pageData,sizeof(DBMainHeaderStruct));
 
 }
 
@@ -16,3 +26,66 @@ DBMainHeaderPage::~DBMainHeaderPage() {
 	// TODO Auto-generated destructor stub
 }
 
+int DBMainHeaderPage::createDBMainHeaderStruct(char *databaseName,int noOfPages,int pageSize,char *pageData){
+	memset(&dbMainHeader_,NULL,sizeof(DBMainHeaderStruct));
+	memset(&dbMainHeader_.genPageHeader_,NULL,sizeof(dbMainHeader_.genPageHeader_));
+	dbMainHeader_.genPageHeader_.pageNumber=0;
+	dbMainHeader_.genPageHeader_.pageType=DB_MAIN_HEADER_PAGE;
+	dbMainHeader_.genPageHeader_.nextPageNumber=-1;
+	memcpy(&dbMainHeader_.dbName_,databaseName,sizeof(databaseName));
+	dbMainHeader_.noOfPages_=noOfPages;
+	dbMainHeader_.noOfTables_=0;
+	dbMainHeader_.pageSize_=pageSize;
+	dbMainHeader_.noOfPagesUsed_= 1;
+	dbMainHeader_.freeStructurePageNumber_= -1;
+	dbMainHeader_.indexCatalogHeaderPageNumber_= -1;
+	dbMainHeader_.sysColumnsHeaderPageNumber_= -1;
+	dbMainHeader_.sysTablesHeaderPageNumber_= -1;
+	memcpy(pageData,&dbMainHeader_,sizeof(dbMainHeader_));
+	return SUCCESS;
+}
+
+int DBMainHeaderPage::getPageNumber(){
+	memcpy(&dbMainHeader_, pageData_, sizeof(DBMainheaderStruct));
+	return dbMainHeader_.sysTableHeaderPageNumber;
+}
+
+int DBMainHeaderPage::getNoOfPagesUsed(){
+	int noOfPagesUsed;
+	memcpy(&noOfPagesUsed,&dbMainHeader_.noOfPagesUsed_,sizeof(int));
+	return noOfPagesUsed;
+}
+
+int DBMainHeaderPage::getSysTablesHeaderPageNumber(){
+	int sysTablesHeaderPageNumber;
+	memcpy(&sysTablesHeaderPageNumber,&dbMainHeader_.sysTablesHeaderPageNumber_,sizeof(int));
+	return sysTablesHeaderPageNumber;
+}
+
+int DBMainHeaderPage::getSysColumnHeaderPageNumber(){
+	int sysColumnsHeaderPageNumber;
+	memcpy(&sysColumnsHeaderPageNumber,&dbMainHeader_.sysColumnsHeaderPageNumber_,sizeof(int));
+	return sysColumnsHeaderPageNumber;
+}
+
+int DBMainHeaderPage::getIndexCatalogHeaderPageNumber(){
+	int indexCatalogHeaderPageNumber;
+	memcpy(&indexCatalogHeaderPageNumber,&dbMainHeader_.indexCatalogHeaderPageNumber_,sizeof(int));
+	return indexCatalogHeaderPageNumber;
+}
+
+int DBMainHeaderPage::getFreeStructurePageNumber(){
+	int freeStructurePageNumber;
+	memcpy(&freeStructurePageNumber,&dbMainHeader_.freeStructurePageNumber_,sizeof(int));
+	return freeStructurePageNumber;
+}
+
+
+void DBMainHeaderPage::setPageNumber(int pageNumber){
+	memcpy(&dbMainHeader_.genPageHeader_.pageNumber,pageNumber,sizeof(int));
+}
+void DBMainHeaderPage::setNoOfPagesUsed(int noOfPagesUsed);
+void DBMainHeaderPage::setSysTablesHeaderPageNumber(int sysTableHeaderPageNumber);
+void DBMainHeaderPage::setSysColumnHeaderPageNumber(int sysColumnsHeaderPageNumber);
+void DBMainHeaderPage::setIndexCatalogHeaderPageNumber(int indexCatalogHeaderPageNumber);
+void DBMainHeaderPage::setFreeStructurePageNumber(int freeStructurePageNumber);
