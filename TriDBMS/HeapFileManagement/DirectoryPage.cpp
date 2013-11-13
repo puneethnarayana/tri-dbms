c1b69ebda63f9c568db821a6ce2226844b05f1b4
alka.luqman@gmail.com <alka.luqman@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-11-13 16:53:43 +0000

    DirectoryPage getters+setters added

--- TriDBMS/HeapFileManagement/DirectoryPage.cpp
+++ TriDBMS/HeapFileManagement/DirectoryPage.cpp
@@ -6,13 +6,102 @@
  */
 
 #include "DirectoryPage.h"
+#include "../Global/globalStructures.h"
+#include "../BufferManagement/BufferManager.h"
+#include "../HeapFileManagement/DataPage.h"
+#include <stdio.h>
+#include <string.h>
 
-DirectoryPage::DirectoryPage() {
+DirectoryPage::DirectoryPage(int fd, int pageNumber) {
 	// TODO Auto-generated constructor stub
-
+	fd_=fd;
+	pageNumber_=pageNumber;
+	buffManager_=BufferManager::getInstance();
+	memset(pageData_,0,sizeof(DEFAULT_PAGE_SIZE));
+	buffManager_->readPage(fd,pageNumber,pageData_);
+	memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	isDirectoryPageChanged_=false;
 }
 
 DirectoryPage::~DirectoryPage() {
 	// TODO Auto-generated destructor stub
+	if(isDirectoryPageChanged_==true){
+		buffManager_=BufferManager::getInstance();
+		buffManager_->writePage(fd_,pageNumber_,pageData_);
+	}
+	delete []pageData_;
 }
 
+
+int DirectoryPage::getNoOfDirectoryEntries(){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	return directoryPageHeader_.noOfDirectoryEntries;
+}
+
+int DirectoryPage::getMaxFreeSpace(){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	return directoryPageHeader_.maxFreeSpace;
+}
+
+int DirectoryPage::getPageNumber(){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	return directoryPageHeader_.genPageHeader_.pageNumber;
+}
+
+int DirectoryPage::getPageType(){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	return directoryPageHeader_.genPageHeader_.pageType;
+}
+
+int DirectoryPage::getNextPageNumber(){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	return directoryPageHeader_.genPageHeader_.nextPageNumber;
+}
+
+void DirectoryPage::setNoOfDirectoryEntries(int noOfDirectoryEntries){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	directoryPageHeader_.noOfDirectoryEntries=noOfDirectoryEntries;
+	memcpy(pageData_,&directoryPageHeader_,sizeof(DirectoryPageHeaderStruct));
+	isDirectoryPageChanged_=true;
+}
+
+void DirectoryPage::setMaxFreeSpace(int maxFreeSpace){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	directoryPageHeader_.maxFreeSpace=maxFreeSpace;
+	memcpy(pageData_,&directoryPageHeader_,sizeof(DirectoryPageHeaderStruct));
+	isDirectoryPageChanged_=true;
+}
+
+void DirectoryPage::setPageNumber(int pageNumber){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	directoryPageHeader_.genPageHeader_.pageNumber=pageNumber;
+	memcpy(pageData_,&directoryPageHeader_,sizeof(DirectoryPageHeaderStruct));
+	isDirectoryPageChanged_=true;
+}
+
+void DirectoryPage::setPageType(int pageType){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	directoryPageHeader_.genPageHeader_.pageType=pageType;
+	memcpy(pageData_,&directoryPageHeader_,sizeof(DirectoryPageHeaderStruct));
+	isDirectoryPageChanged_=true;
+}
+
+void DirectoryPage::setNextPageNumber(int nextPageNumber){
+	//memcpy(&directoryPageHeader_,pageData_,sizeof(DirectoryPageHeaderStruct));
+	directoryPageHeader_.genPageHeader_.nextPageNumber=nextPageNumber;
+	memcpy(pageData_,&directoryPageHeader_,sizeof(DirectoryPageHeaderStruct));
+	isDirectoryPageChanged_=true;
+}
+
+int DirectoryPage::createDirectoryPage(int pageNumber, char *pageData){
+	directoryPageHeader_.genPageHeader_.pageNumber=pageNumber;
+	directoryPageHeader_.genPageHeader_.nextPageNumber=-1;
+	directoryPageHeader_.genPageHeader_.pageType=DIRECTORY_PAGE;
+	//directoryPageHeader_.maxFreeSpace=DEFAULT_PAGE_SIZE-getDataPageSize();
+	directoryPageHeader_.noOfDirectoryEntries=
+			(DEFAULT_PAGE_SIZE-sizeof(DirectoryPageHeaderStruct))/8;
+	//Directory Entry = page number + free space
+	memcpy(pageData_,&directoryPageHeader_,sizeof(directoryPageHeader_));
+	isDirectoryPageChanged_=true;
+	return SUCCESS;
+}
--- TriDBMS/HeapFileManagement/DirectoryPage.h
+++ TriDBMS/HeapFileManagement/DirectoryPage.h
@@ -8,10 +8,28 @@
 #ifndef DIRECTORYPAGE_H_
 #define DIRECTORYPAGE_H_
 
+#include "../Global/globalStructures.h"
+#include "../BufferManagement/BufferManager.h"
+
 class DirectoryPage {
 public:
-	DirectoryPage();
+	DirectoryPage(int fd, int pageNumber);
 	virtual ~DirectoryPage();
+
+	int getNoOfDirectoryEntries();
+	int getMaxFreeSpace();
+	int getPageNumber();
+	int getPageType();
+	int getNextPageNumber();
+
+	void setNoOfDirectoryEntries(int noOfDirectoryEntries);
+	void setMaxFreeSpace(int maxFreeSpace);
+	void setPageNumber(int pageNumber);
+	void setPageType(int pageType);
+	void setNextPageNumber(int nextPageNumber);
+
+	int createDirectoryPage(int pageNumber, char *pageData);
+
 private:
 	typedef struct{
 		GenPageHeaderStruct genPageHeader_;
@@ -19,6 +37,11 @@ private:
 		int maxFreeSpace;
 	}DirectoryPageHeaderStruct;
 	DirectoryPageHeaderStruct directoryPageHeader_;
+	BufferManager *buffManager_;
+	int fd_;
+	int pageNumber_;
+	char *pageData_;
+	bool isDirectoryPageChanged_;
 };
 
 #endif /* DIRECTORYPAGE_H_ */
