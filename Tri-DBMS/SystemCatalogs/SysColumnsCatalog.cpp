9018c3408a0f0153b6f63398f9a963b0ee7c2058
alka.luqman@gmail.com <alka.luqman@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-11-13 23:05:29 +0000

    sysColumn added sysColumnEntry

--- Tri-DBMS/SystemCatalogs/SysColumnsCatalog.cpp
+++ Tri-DBMS/SystemCatalogs/SysColumnsCatalog.cpp
@@ -6,13 +6,48 @@
  */
 
 #include "SysColumnsCatalog.h"
+#include "../BufferManagement/BufferManager.h"
+#include "../Global/globalStructures.h"
+#include "../Global/globalDefines.h"
 
-SysColumnsCatalog::SysColumnsCatalog() {
+SysColumnsCatalog::SysColumnsCatalog(int fd, int pageNumber) {
 	// TODO Auto-generated constructor stub
-
+	fd_=fd;
+	pageNumber_=pageNumber;
+	buffManager_=BufferManager::getInstance();
+	memset(pageData_,0,sizeof(DEFAULT_PAGE_SIZE));
+	buffManager_->readPage(fd,pageNumber,pageData_);
+	maxSysColumnEntriesPerPage_=
+			(DEFAULT_PAGE_SIZE-sizeof(GenPageHeaderStruct)-sizeof(int))/sizeof(sysColumnPageStruct);
+	memset(sysColumnPage_.sysColumnEntry_,0,maxSysColumnEntriesPerPage_);
+	memcpy(&sysColumnPage_,pageData_,sizeof(sysColumnPageStruct));
+	isSysColumnChanged_=false;
 }
 
 SysColumnsCatalog::~SysColumnsCatalog() {
 	// TODO Auto-generated destructor stub
+	if(isSysColumnChanged_ ==true){
+			buffManager_=BufferManager::getInstance();
+			buffManager_->writePage(fd_,pageNumber_,pageData_);
+		}
+		delete []pageData_;
+}
+
+int SysColumnsCatalog::insertSysColumnEntry(char *columnName, char *tableName, int columnPosition, int columnType, char *pageData){
+
+	sysColumnPage_.genPageHeader_.pageNumber=pageNumber_;
+	sysColumnPage_.genPageHeader_.pageType=SYS_COLUMNS_PAGE;
+	sysColumnPage_.genPageHeader_.nextPageNumber=-1;
+
+	memcpy(sysColumnPage_.sysColumnEntry_[sysColumnPage_.noOfEntries].columnName_,columnName,MAX_FILE_NAME_LENGTH);
+	memcpy(sysColumnPage_.sysColumnEntry_[sysColumnPage_.noOfEntries].tableName_,tableName,MAX_FILE_NAME_LENGTH);
+	sysColumnPage_.sysColumnEntry_[sysColumnPage_.noOfEntries].columnPosition_=columnPosition;
+	sysColumnPage_.sysColumnEntry_[sysColumnPage_.noOfEntries].columnType_=columnType;
+	sysColumnPage_.noOfEntries++;
+	memcpy(pageData_,&sysColumnPage_,sizeof(sysColumnPageStruct));
+	memcpy(&pageData,&sysColumnPage_,sizeof(sysColumnPageStruct));
+
+	isSysColumnChanged_=true;
+	return SUCCESS;
 }
 
--- Tri-DBMS/SystemCatalogs/SysTablesCatalog.h
+++ Tri-DBMS/SystemCatalogs/SysTablesCatalog.h
@@ -12,32 +12,32 @@
 #include "../BufferManagement/BufferManager.h"
 class SysTablesCatalog {
 public:
-	SysTablesCatalog(int fd,int pageNumber);
-	virtual ~SysTablesCatalog();
-	//int createSysTableEntry(char *tableName,int recordSize,int noOfColumns,int dpChainHeader);
-	int insertSysTableEntry(char *tableName,int recordSize,int noOfColumns,int dpChainHeader,char *pageData);
+        SysTablesCatalog(int fd,int pageNumber);
+        virtual ~SysTablesCatalog();
+        //int createSysTableEntry(char *tableName,int recordSize,int noOfColumns,int dpChainHeader);
+        int insertSysTableEntry(char *tableName,int recordSize,int noOfColumns,int dpChainHeader,char *pageData);
 private:
 
-	typedef struct{
-		char* tableName_;
-		int recordSize_;
-		int noOfColumns_;
-		int dpChainHeaderAddress_;
-	}SysTableEntryStruct;
+        typedef struct{
+                char* tableName_;
+                int recordSize_;
+                int noOfColumns_;
+                int dpChainHeaderAddress_;
+        }SysTableEntryStruct;
 
-	typedef struct{
-		GenPageHeaderStruct genPageHeader_;
-		int noOfEntries;
-		SysTableEntryStruct *sysTableEntries_;
-	}sysTablePageStruct;
+        typedef struct{
+                GenPageHeaderStruct genPageHeader_;
+                int noOfEntries;
+                SysTableEntryStruct *sysTableEntries_;
+        }sysTablePageStruct;
 
-	sysTablePageStruct sysTablePage_;
-	int fd_;
-	int pageNumber_;
-	char *pageData_;
-	BufferManager *buffManager_;
-	bool isSysTableChanged_;
-	int maxSysTableEntriesPerPage_;
+        sysTablePageStruct sysTablePage_;
+        int fd_;
+        int pageNumber_;
+        char *pageData_;
+        BufferManager *buffManager_;
+        bool isSysTableChanged_;
+        int maxSysTableEntriesPerPage_;
 };
 
 #endif /* SYSTABLESCATALOG_H_ */
