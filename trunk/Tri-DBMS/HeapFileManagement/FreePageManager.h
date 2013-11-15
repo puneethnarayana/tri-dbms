045a17d9385bce239ea4774bf0d62d22bcf3ff1e
ravindrabhavanam@gmail.com <ravindrabhavanam@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-11-15 20:13:25 +0000

    FreePageManager Implementation done!!

--- trunk/Tri-DBMS/HeapFileManagement/FreePageManager.h
+++ trunk/Tri-DBMS/HeapFileManagement/FreePageManager.h
@@ -7,19 +7,34 @@
 
 #ifndef FREEPAGEMANAGER_H_
 #define FREEPAGEMANAGER_H_
-
+#include "../BufferManagement/BufferManager.h"
 class FreePageManager {
 public:
-	FreePageManager(int totalNoOfPages);
+	FreePageManager(int fd,int pageNumber);
 	virtual ~FreePageManager();
+	int createFreePageManagerPage(int pageNumber,char *pageData);
+	int setPage(int pageNumber); // 0 means free, 1 means allocated.
+	int freePage(int pageNumber);
+	bool isPageFree(int pageNumber);
 	int getFreePage();
 private:
+	//	typedef struct{
+	//		bool isFree_;
+	//	}freePageStruct;
 
 	typedef struct{
-		bool isFree_;
+		GenPageHeaderStruct genPageHeader_;
+		int maxNoOfPages_;
+		int noOfFreePages_;
+		int headerOffset_;
 	}freePageStruct;
 
-	freePageStruct *freePageManager;
+	freePageStruct freePageManager_;
+	BufferManager *buffManager_;
+	int fd_;
+	int pageNumber_;
+	char *pageData_;
+	bool isFreePageManagerChanged_;
 };
 
 #endif /* FREEPAGEMANAGER_H_ */
