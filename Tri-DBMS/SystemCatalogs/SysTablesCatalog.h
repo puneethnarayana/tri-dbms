894ad46c0fd0fbaf8febdf8f01491b28ccd00344
ravindrabhavanam@gmail.com <ravindrabhavanam@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-11-20 13:42:19 +0000

    get Schema in syscolumn; get no of columns, dp chain header are added in systable;

--- Tri-DBMS/SystemCatalogs/SysTablesCatalog.h
+++ Tri-DBMS/SystemCatalogs/SysTablesCatalog.h
@@ -10,12 +10,19 @@
 #include "../Global/globalStructures.h"
 #include "../Global/globalDefines.h"
 #include "../BufferManagement/BufferManager.h"
+#include <vector>
+#include <string>
+using namespace std;
 class SysTablesCatalog {
 public:
         SysTablesCatalog(int fd,int pageNumber);
         virtual ~SysTablesCatalog();
         int createSysTablePage(int pageNumber,char *pageData);
         int insertSysTableEntry(char *tableName,int maxRecordSize,int noOfColumns,int dpChainHeader,char *pageData);
+        vector<string> getSysTableRecordAsVector(char *tableName);
+        int getDPChainHeaderPageNumber(char *tableName);
+        int getNoOfColumns(char *tableName);
+        int getMaxRecordSize(char *tableName);
 private:
         typedef struct{
 
