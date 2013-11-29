1d1e6a39129db87e49aa146c8f67fdbf982aab70
ravindrabhavanam@gmail.com <ravindrabhavanam@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-11-29 21:26:42 +0000

    Added postFixEvaluator != operation and corrected few things

--- Tri-DBMS/Utils/PostFixEvaluator.cpp
+++ Tri-DBMS/Utils/PostFixEvaluator.cpp
@@ -10,6 +10,9 @@
 #include <stack>
 #include "Record.h"
 #include "WhereExpressionElement.h"
+#include <stdio.h>
+#include <iostream>
+using namespace std;
 PostFixEvaluator::PostFixEvaluator(Record& tuple) {
 	// TODO Auto-generated constructor stub
 	tuple_=tuple;
@@ -25,14 +28,17 @@ bool PostFixEvaluator::evaluate(std::vector<WhereExpressionElement> exprList) {
 	for (unsigned i = 0; i < exprList.size(); i++) {
 		if (exprList[i].type_ == WhereExpressionElement::IDENTIFIER_TYPE || exprList[i].type_
 				== WhereExpressionElement::LITERAL_TYPE) {
+			//cout << "inside id or literal evaluate: " << exprList[i].identifierValue << " " << exprList[i].literalValue<< endl;
 			myStack.push(exprList[i]);
 		} else {
+			//cout << "inside operator evaluate" << endl;
 			WhereExpressionElement op = exprList[i];
 			WhereExpressionElement exp1 = myStack.top();
 			myStack.pop();
 			WhereExpressionElement exp2 = myStack.top();
 			myStack.pop();
 			WhereExpressionElement res = eval(exp1, exp2, op);
+			//cout << "expression :" << exp1.literalValue << " "<<exp2.identifierValue << " " <<op.operatorValue << endl;
 			myStack.push(res);
 		}
 	}
@@ -45,21 +51,46 @@ WhereExpressionElement PostFixEvaluator::eval(WhereExpressionElement exp1, Where
 	res.type_ = WhereExpressionElement::RESULT_TYPE;
 	if (op.operatorValue == "=") {
 		if (exp1.type_ == WhereExpressionElement::IDENTIFIER_TYPE) {
+			//cout << "1"<< endl;
 			if (tuple_.fieldMatchByConversion(exp1.identifierValue,
 					exp2.literalValue)==true) {
 				res.res = true;
 			}
 		} else if (exp2.type_ == WhereExpressionElement::IDENTIFIER_TYPE) {
+			//cout << "2"<< endl;
 			if (tuple_.fieldMatchByConversion(exp2.identifierValue,
 					exp1.literalValue)==true) {
+					//cout << "2 2" << endl;
 					res.res = true;
 			}
 		} else if (exp1.type_ == WhereExpressionElement::RESULT_TYPE) {
+			//cout << "3"<< endl;
 			if(exp1.res == exp2.res){
 				res.res = true;
 			}
 		}
-	}else 	if (op.operatorValue == "<") {
+	}else if (op.operatorValue == "!=") {
+		if (exp1.type_ == WhereExpressionElement::IDENTIFIER_TYPE) {
+			//cout << "1"<< endl;
+			if (tuple_.fieldMatchByConversion(exp1.identifierValue,
+					exp2.literalValue)==false) {
+				res.res = true;
+			}
+		} else if (exp2.type_ == WhereExpressionElement::IDENTIFIER_TYPE) {
+			//cout << "2"<< endl;
+			if (tuple_.fieldMatchByConversion(exp2.identifierValue,
+					exp1.literalValue)==false) {
+					//cout << "2 2" << endl;
+					res.res = true;
+			}
+		} else if (exp1.type_ == WhereExpressionElement::RESULT_TYPE) {
+			//cout << "3"<< endl;
+			if(exp1.res != exp2.res){
+				res.res = true;
+			}
+		}
+	}
+	else 	if (op.operatorValue == "<") {
 		if (exp1.type_ == WhereExpressionElement::IDENTIFIER_TYPE) {
 			if (tuple_.lessThanByConversion(exp1.identifierValue,
 					exp2.literalValue)==true) {
