77929d2ddb2323f7b2f586bde639b52b4ca2fd32
ravindrabhavanam@gmail.com <ravindrabhavanam@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-11-29 21:46:24 +0000

    SysColumnCatalog. one more field for columnSizes

--- trunk/Tri-DBMS/DatabaseManagement/DatabaseOperations.h
+++ trunk/Tri-DBMS/DatabaseManagement/DatabaseOperations.h
@@ -31,7 +31,7 @@ public:
 	int createDatabase(char *databaseName,int databaseSize);
 	int openDatabase(char *databaseName);
 	int closeDatabase(int fd);
-	int createTable(char *tableName, vector<string> columnList, vector<string> columnTypeList);
+	int createTable(char *tableName, vector<string> columnList, vector<string> columnTypeList,vector<string> columnSizeList);
 	int insertIntoTable(char *tableName, vector<string> insertValues);
 	int selectAllFromTable(char *tableName, vector<string> columnList);
 	int dropTable(char *tableName);
