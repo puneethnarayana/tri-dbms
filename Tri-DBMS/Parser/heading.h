eabc6d3fef84b2b28eba700eec090b6ea9d7ff3f
alka.luqman@gmail.com <alka.luqman@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-12-02 14:53:34 +0000

    create db, create table, insert working!

--- Tri-DBMS/Parser/heading.h
+++ Tri-DBMS/Parser/heading.h
@@ -8,12 +8,45 @@
 #ifndef HEADING_H_
 #define HEADING_H_
 
-#include <cstdio>
+#include <cstring>
 #include <iostream>
-#include <stdio.h>
+#include <istream>
+#include <string>
+#include <stdlib.h>
+
 using namespace std;
+#include "../DatabaseManagement/QueryStructure.h"
+#include "../Utils/CommonUtil.h"
+#include "../Utils/DataTypeLookup.h"
+#include "../Utils/FieldLengthLookup.h"
+#include "../DatabaseManagement/DatabaseOperations.h"
 
-#include "../Global/globalVariables.h"
 
+extern string globalDatabaseName;
+extern string globalTableName;
+extern string globalIndexName;
+extern vector<string> globalColumnNameList;
+extern vector<string> globalColumnValueList;
+extern vector<string> globalColumnDatatypeList;
+extern vector<string> globalColumnDatasizeList;
+extern vector<string> globalWhereList;
+
+extern int globalStatementType;
+
+
+static const int CREATE_DATABASE_STATEMENT = 0;
+static const int SHOW_DATABASES_STATEMENT = 1;
+static const int OPEN_DATABASE_STATEMENT = 2;
+static const int DROP_DATABASE_STATEMENT = 3;
+static const int CREATE_TABLE_STATEMENT = 4;
+static const int INSERT_STATEMENT = 5;
+static const int SELECT_STATEMENT = 6;
+static const int DELETE_STATEMENT = 7;
+static const int UPDATE_STATEMENT = 8;
+static const int DROP_TABLE_STATEMENT = 9;
+static const int CREATE_INDEX_STATEMENT = 10;
+static const int DROP_INDEX_STATEMENT = 11;
+static const int SHOW_TABLES_STATEMENT = 12;
+static const int DESCRIBE_STATEMENT = 13;
 
 #endif /* HEADING_H_ */
