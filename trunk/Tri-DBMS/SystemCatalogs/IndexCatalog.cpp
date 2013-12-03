0703af9c7b64eff116642ab249798c9afd7841e9
ravindrabhavanam@gmail.com <ravindrabhavanam@gmail.com@5b46069f-67c0-53ac-c31b-8659095eb966>
2013-12-03 08:02:07 +0000

    IndexCatalog Finished! :D:D

--- trunk/Tri-DBMS/SystemCatalogs/IndexCatalog.cpp
+++ trunk/Tri-DBMS/SystemCatalogs/IndexCatalog.cpp
@@ -6,18 +6,77 @@
  */
 
 #include "IndexCatalog.h"
+#include "../BufferManagement/BufferManager.h"
+#include "../Global/globalStructures.h"
 #include "../Global/globalDefines.h"
-IndexCatalog::IndexCatalog() {
+#include "../HeapFileManagement/DataPage.h"
+#include "../Utils/CommonUtil.h"
+#include "../Utils/Record.h"
+#include <vector>
+#include <string>
+using namespace std;
+IndexCatalog::IndexCatalog(int fd,int pageNumber) {
 	// TODO Auto-generated constructor stub
-
+	fd_=fd;
+	pageNumber_=pageNumber;
+	pageData_=new char[DEFAULT_PAGE_SIZE];
+	buffManager_ = BufferManager::getInstance();
+	memset(pageData_,0,DEFAULT_PAGE_SIZE);
+	buffManager_->readPage(fd,pageNumber,pageData_);
+	isIndexCatalogChanged_=false;
 }
 
 IndexCatalog::~IndexCatalog() {
 	// TODO Auto-generated destructor stub
+//	if(isIndexCatalogChanged_==true){
+//		buffManager_->writePage(fd_,pageNumber_,pageData_);
+//	}
+	delete[] pageData_;
 }
 
 
 int IndexCatalog::createIndexCatalogPage(int pageNumber){
-
+	DataPage *indexCatalogPage=new DataPage(fd_,pageNumber_);
+	indexCatalogPage->createDataPageHeaderStruct(pageNumber_);
+	indexCatalogPage->setPageType(INDEX_CATALOG_PAGE);
+	delete indexCatalogPage;
 	return SUCCESS;
 }
+
+
+
+
+
+int IndexCatalog::insertIndexEntry(char *indexName, char *tableName, char *indexAttribute, int indexType, int keySize,int indexHeaderPageNo){
+
+// Code needs to be written here!!!
+	vector<string> values;
+	int indexAttributeLength_=0;
+	char *recordString=new char[DEFAULT_PAGE_SIZE];
+	memset(recordString,0,DEFAULT_PAGE_SIZE);
+	Record *record=new Record();
+	values.push_back(indexName);
+	values.push_back(CommonUtil::int_to_string(indexType));
+	values.push_back(CommonUtil::int_to_string(keySize));
+	values.push_back(CommonUtil::int_to_string(indexHeaderPageNo));
+	values.push_back(tableName);
+	values.push_back(indexAttribute);
+
+
+	record->getRecordString(values,recordString,&indexAttributeLength_);
+	DataPage *indexPage=new DataPage(fd_,pageNumber_);
+	//buffManager_->hexDump(recordString);
+	//cout << "len of index attr:"<<indexAttributeLength_<<endl;
+	indexPage->insertRecord(recordString,indexAttributeLength_);
+	isIndexCatalogChanged_=true;
+	//cout << "after inserting into index catalog"<<endl;
+//	buffManager_->commitCache();
+//	buffManager_->hexDump(fd_,4);
+	delete[] recordString;
+	delete indexPage;
+	delete record;
+	values.clear();
+	return SUCCESS;
+}
+
+
--- trunk/Tri-DBMS/SystemCatalogs/IndexCatalog.h
+++ trunk/Tri-DBMS/SystemCatalogs/IndexCatalog.h
@@ -7,12 +7,26 @@
 
 #ifndef INDEXCATALOG_H_
 #define INDEXCATALOG_H_
-
+#include "../Global/globalStructures.h"
+#include "../Global/globalDefines.h"
+#include "../BufferManagement/BufferManager.h"
+#include <vector>
+#include <string>
+using namespace std;
 class IndexCatalog {
 public:
-	IndexCatalog();
+	IndexCatalog(int fd,int pageNumber);
 	virtual ~IndexCatalog();
 	int createIndexCatalogPage(int pageNumber);
+	int insertIndexEntry(char *indexName, char *tableName, char *indexKey, int indexType,int keySize,int indexHeaderPageNo);
+
+private:
+
+	        int fd_;
+	        int pageNumber_;
+	        char *pageData_;
+	        BufferManager *buffManager_;
+	        bool isIndexCatalogChanged_;
 };
 
 #endif /* INDEXCATALOG_H_ */
